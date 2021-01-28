f7f8b8cb9b6e56dbeea3cf3b92e5d7b668d2fad8
Boris Grunwald <borisgrunwald@Boriss-MacBook-Pro.local>
2021-01-28 23:29:56 +0100

    WIP

--- app.ts
+++ app.ts
@@ -1,3 +1,31 @@
-const divisionsBox = document.getElementById('divisions') as HTMLDivElement;
+import * as Tone from 'tone';
+
+document.getElementById('play-button')!.addEventListener('click', () => {
+    toggleMetronome();
+})
+
+Tone.Transport.bpm.value = 120;
+Tone.Transport.start();
+
+
+const synth = new Tone.Synth({
+    
+}).toDestination();
+let metronomeIsRunning = false;
+let loop = new Tone.Loop(time => {
+    synth.triggerAttackRelease("C3", "8n", time);
+}, "4n")
+
+function toggleMetronome() {
+    if(!metronomeIsRunning) {
+        loop.start(0)
+        metronomeIsRunning = true;
+    } else {
+        loop.stop()
+        metronomeIsRunning = false;
+    }
+}
+
+
 
 
